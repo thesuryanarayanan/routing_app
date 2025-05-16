11fdc05b7913254b45ecf91cb147dd81168d7e4e
Eren <>
2025-05-17 00:48:22 +0300

    no message

--- routing_backend/src/main/java/com/example/routing_backend/config/DataInitializerConfig.java
+++ routing_backend/src/main/java/com/example/routing_backend/config/DataInitializerConfig.java
@@ -2,17 +2,26 @@ package com.example.routing_backend.config;
 
 import com.example.routing_backend.entity.Branch;
 import com.example.routing_backend.entity.Customer;
+import com.example.routing_backend.entity.Dispatch;
 import com.example.routing_backend.entity.Vehicle;
+import com.example.routing_backend.enums.DeliveryRange;
+import com.example.routing_backend.enums.DispatchType;
 import com.example.routing_backend.enums.VehicleType;
 import com.example.routing_backend.repository.BranchRepository;
 import com.example.routing_backend.repository.CustomerRepository;
+import com.example.routing_backend.repository.DispatchRepository;
 import com.example.routing_backend.repository.VehicleRepository;
 import com.example.routing_backend.utility.DateUtility;
 import lombok.RequiredArgsConstructor;
+import org.bouncycastle.crypto.agreement.srp.SRP6Client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Configuration
 @RequiredArgsConstructor
@@ -21,6 +30,7 @@ public class DataInitializerConfig {
     private final BranchRepository branchRepository;
     private final VehicleRepository vehicleRepository;
     private final CustomerRepository customerRepository;
+    private final DispatchRepository dispatchRepository;
 
     @Value("${spring.jpa.hibernate.ddl-auto}")
     private String ddlAuto;
@@ -59,6 +69,27 @@ public class DataInitializerConfig {
                     customer.setPhone("5555555555");
                     customerRepository.save(customer);
                 }
+
+                List<HashMap<String,Double>> coordinates = List.of(
+                        new HashMap<>(Map.of("latitude", 41.09768333375933, "longitude", 28.88992153956545)),
+                        new HashMap<>(Map.of("latitude", 41.1002360511431, "longitude", 28.89185302123544)),
+                        new HashMap<>(Map.of("latitude", 41.09779438743708, "longitude", 28.885973619325004)),
+                        new HashMap<>(Map.of("latitude", 41.10138408681063, "longitude", 28.880931066591593)),
+                        new HashMap<>(Map.of("latitude", 41.10196673483382, "longitude", 28.877557886397234))
+                );
+
+                for (int i = 0; i < 5; i++) {
+                    Dispatch dispatch = new Dispatch();
+                    dispatch.setCustomer(customerRepository.findById((long) (i + 1)).orElse(null));
+                    dispatch.setDispatchType(DispatchType.FILE);
+                    dispatch.setReceiverLatitude(coordinates.get(i).get("latitude"));
+                    dispatch.setReceiverLongitude(coordinates.get(i).get("longitude"));
+                    dispatch.setReceiverAddress("Sanko Park AVM");
+                    dispatch.setDeliveryRange(DeliveryRange.EVENING);
+                    dispatch.setWeight(0.5);
+                    dispatchRepository.save(dispatch);
+                }
+
                 System.out.println("Başlangıç verileri başarıyla eklendi.");
             }
 
